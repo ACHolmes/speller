f3b7b166b53e0192d4e6173db792145942a494b5
Andrew Holmes <acholmes001@gmail.com>
2024-01-12 21:12:11 -0500

    Branch for bloom filter investigation. Seems like whatever I do I get 17069, not sure exactly why

--- dictionary.c
+++ dictionary.c
@@ -9,8 +9,8 @@
 #include "string.h"
 
 #define NODE_CAPACITY 5
-#define base1 400000
-#define base2 800000 
+#define base1 300043
+#define base2 2 * base1 
 // #define N 1200000
 
 
@@ -19,7 +19,7 @@
 __uint32_t dictsize = 0;
 
 
-const unsigned int N = 1200000;
+const unsigned int N = 3 * base1;
 
 // Hash table
 __uint8_t table[N];
@@ -27,13 +27,18 @@ __uint8_t table[N];
 inline static void hash_set_all (const char* word) {
     unsigned long hash1 = 5381;
     unsigned long hash2 = 0;
-    unsigned long hash3 = 0;
+    unsigned long hash3 = 0x12345678;
+
+    // See: https://github.com/aappleby/smhasher/blob/master/src/Hashes.cpp
+    hash3 ^= 2166136261UL;
+
     int c;
 
     while ((c = *word++)) {
         hash1 = ((hash1 << 5) + hash1) + c;
         hash2 = c + (hash2 << 6) + (hash2 << 16) - hash2;
-        hash3 += c;
+        hash3 ^= c;
+        hash3 *= 16777619;
     }
     table[hash1 % base1] = 1;
     table[base1 + (hash2 % base1)] = 1;
@@ -44,13 +49,18 @@ inline static void hash_set_all (const char* word) {
 inline static bool hash_check_all (const char* word) {
     unsigned long hash1 = 5381;
     unsigned long hash2 = 0;
-    unsigned long hash3 = 0;
+    unsigned long hash3 = 0x12345678;
+
+    // See: https://github.com/aappleby/smhasher/blob/master/src/Hashes.cpp
+    hash3 ^= 2166136261UL;
+
     int c;
 
     while ((c = *word++)) {
         hash1 = ((hash1 << 5) + hash1) + c;
         hash2 = c + (hash2 << 6) + (hash2 << 16) - hash2;
-        hash3 += c;
+        hash3 ^= c;
+        hash3 *= 16777619;
     }
     return table[hash1 % base1] & table[base1 + (hash2 % base1)] & table[base2 + (hash3 % base1)];
 }
