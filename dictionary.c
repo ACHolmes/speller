6a37bd9b0d1a8313f5a7a2bf9e1a436c064dc39a
Andrew Holmes <acholmes001@gmail.com>
2024-01-14 14:37:53 -0500

    2 hash function solution, honestly surpsingly slow to me

--- dictionary.c
+++ dictionary.c
@@ -14,8 +14,8 @@
 
 #define NODE_CAPACITY 5
 #define BASE_HASH3 0x12345678 ^ 2166136261UL
-#define TABLE_SIZE 400000
-#define BLOOM_FILTER_SIZE (5 * TABLE_SIZE)
+#define TABLE_SIZE 2100013
+#define BLOOM_FILTER_SIZE (2 * TABLE_SIZE)
 
 const unsigned int N = BLOOM_FILTER_SIZE;
 
@@ -32,53 +32,58 @@ bloom_filter* bf;
 // Storing bool whether persisted to avoid msync always
 bool persisted = false;
 
+__uint8_t* table1, *table2;
+
 inline static void hash_set_all (const char* word) {
-    unsigned long hash1 = 5381;
-    unsigned long hash2 = 3;
-    unsigned long hash3 = BASE_HASH3;
-    unsigned long hash4 = 119;
-    unsigned long hash5 = 0;
+    unsigned long hash1 = 5387;
+    unsigned long hash2 = 2;
+    // unsigned long hash3 = BASE_HASH3;
+    // unsigned long hash4 = 119;
+    // unsigned long hash5 = 0;
 
     int c;
-    int i = 0;
+    // int i = 0;
     while ((c = *word++)) {
-        hash1 = ((hash1 << 5) + hash1) + c;
+        hash1 = ((hash1 << 5) + (hash1 << 2)) + c;
         hash2 = c + (hash2 << 6) + (hash2 << 16) - hash2;
-        hash3 ^= c;
-        hash3 *= 16777693;
-        hash4 = hash4 * 493 + c;
-        hash5 += (c << (5 * i++)) + 513;
+        // hash3 ^= c;
+        // hash3 *= 16777697;
+        // hash4 = hash4 * 493 + c;
+        // hash5 += (c << (5 * i++)) + 513;
     }
 
-    bf->store[hash1 % TABLE_SIZE] = 1;
-    bf->store[TABLE_SIZE + hash2 % TABLE_SIZE] = 1;
-    bf->store[TABLE_SIZE * 2 + hash3 % TABLE_SIZE] = 1;
-    bf->store[TABLE_SIZE * 3 + hash4 % TABLE_SIZE] = 1; 
-    bf->store[TABLE_SIZE * 4 + hash5 % TABLE_SIZE] = 1; 
+    table1[hash1 % TABLE_SIZE] = 1;
+    table2[hash2 % TABLE_SIZE] = 1;
+    // table3[hash3 % TABLE_SIZE] = 1;
+    // table4[hash4 % TABLE_SIZE] = 1; 
+    // table5[hash5 % TABLE_SIZE] = 1; 
     return;
 }
 
 inline static bool hash_check_all (const char* word) {
-    unsigned long hash1 = 5381;
-    unsigned long hash2 = 3;
-    unsigned long hash3 = BASE_HASH3;
-    unsigned long hash4 = 119;
-    unsigned long hash5 = 0;
+    unsigned long hash1 = 5387;
+    unsigned long hash2 = 2;
+    // unsigned long hash3 = BASE_HASH3;
+    // unsigned long hash4 = 119;
+    // unsigned long hash5 = 0;
 
     int c;
-    int i = 0;
+    // int i = 0;
 
     while ((c = *word++)) {
-        hash1 = ((hash1 << 5) + hash1) + c;
+        hash1 = ((hash1 << 5) + (hash1 << 2)) + c;
         hash2 = c + (hash2 << 6) + (hash2 << 16) - hash2;
-        hash3 ^= c;
-        hash3 *= 16777693;
-        hash4 = hash4 * 493 + c;
-        hash5 += (c << (5 * i++)) + 513;
+        // hash3 ^= c;
+        // hash3 *= 16777697;
+        // hash4 = hash4 * 493 + c;
+        // hash5 += (c << (5 * i++)) + 513;
 
     }
-    return bf->store[hash1 % TABLE_SIZE] & bf->store[TABLE_SIZE + hash2 % TABLE_SIZE] & bf->store[2 * TABLE_SIZE + hash3 % TABLE_SIZE]
-        &  bf->store[3 * TABLE_SIZE + hash4 % TABLE_SIZE] & bf->store[4 * TABLE_SIZE + hash5 % TABLE_SIZE];
+    return table1[hash1 % TABLE_SIZE] & table2[hash2 % TABLE_SIZE] 
+    // & table3[hash3 % TABLE_SIZE]
+        // &  table4[hash4 % TABLE_SIZE] 
+        // & table5[hash5 % TABLE_SIZE]
+        ;
 }
 
 // Returns true if word is in dictionary, else false
@@ -103,6 +108,7 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
+    __uint32_t dictsize = 0;
     // Setup path to the persisted hash table
     char path[LENGTH + 10];
     sprintf(path, "%s.datas",  dictionary);
@@ -112,9 +118,10 @@ bool load(const char *dictionary)
         int fd = open(path, O_RDWR | O_CREAT, (mode_t)0600);
 
         bf = (bloom_filter*) mmap(0,(sizeof(bloom_filter)), PROT_READ, MAP_SHARED, fd, 0);
+        table1 = &bf->store[0];
+        table2 = &bf->store[TABLE_SIZE];
         close (fd);
         persisted = true;
-        printf("size: %i\n", bf->size);
         return true;
     }
 
@@ -123,6 +130,8 @@ bool load(const char *dictionary)
     lseek(fd, (sizeof(bloom_filter)-1), SEEK_SET);
     write(fd, "", 1);
     bf = (bloom_filter*) mmap(0,(sizeof(bloom_filter)), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+    table1 = &bf->store[0];
+    table2 = &bf->store[TABLE_SIZE];
     close(fd);
 
     // Open dictionary
@@ -133,9 +142,9 @@ bool load(const char *dictionary)
     while (fgets(buffer, LENGTH + 2, f)) {
         buffer[strlen(buffer) - 1] = 0;
         hash_set_all(buffer); 
-        bf->size++;
+        dictsize++;
     }
-
+    bf->size = dictsize;
     // Close for cleanup and store dictsize in persisted hash table
     fclose(f);
     return true;
